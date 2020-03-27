ca25792aea04edc2146279d04bc540065af15280
Thophile <montemonttheophile@gmail.com>
2020-03-27 11:52:10 +0100

    Addition of the css folder link

--- views/base.php
+++ views/base.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <!--<script src="<?php $_SERVER['DOCUMENT_ROOT'].'assets/js'?>"></script>-->
+    <!--<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'].'assets/css'?>">-->
 </head>
 <body>
     <header>
