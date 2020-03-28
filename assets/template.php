2ddd8ff35251eff433dc6a2c7b33fdf665585464
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-28 18:49:24 +0100

    changed template to php for the colors

--- assets/template.php
+++ assets/template.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta author="Thophile">
     <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/all.css">
     <title><?= $title ?></title>
 </head>
 <body>
