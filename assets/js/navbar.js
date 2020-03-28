35439b6f29221d1563d71a45ab0ac2f949e4bca5
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-28 22:06:22 +0100

    Added fade out on the navbar when scrolling down

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,31 @@
 var sidenav = false;
-
+var currscroll;
+var pastscroll = 0;
 function toggleNav(){
     if (sidenav) {
-        document.getElementById('side-nav').style.marginLeft = "-250px"
-        document.getElementById('main').style.marginLeft = "0px"
-        console.log(sidenav)
+        document.getElementById('side-nav').style.left = "-250px"
         sidenav = !sidenav
     }else{
-        document.getElementById('side-nav').style.marginLeft = "0"
-        document.getElementById('main').style.marginLeft = "250px"
-        console.log(sidenav)
+        document.getElementById('side-nav').style.left = "0"
         sidenav = !sidenav
 
     }
 }
+function hideNav(){
+    currscroll = window.scrollY;
+    if((currscroll > pastscroll ) & !sidenav){
+        document.getElementById('navbar').style.top = "-50px"
+        console.log('scrollin down')
+    }else{
+        document.getElementById('navbar').style.top = "0px"
+        console.log('scrollin up')
+    }
+    pastscroll = currscroll;
+}
+
+
+ready(function() {
+    window.onscroll = function(){
+        hideNav()
+    }
+})
