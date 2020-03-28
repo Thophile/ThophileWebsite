bf807b2076daafae0a10dcf2bcd035a76ccc4fcb
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-28 17:03:04 +0100

    Copied templtate to projects page

---
+++ views/projects.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Thophile">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <header>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/navbar.html'?>
+    </header>
+    <main></main>
+</body>
+</html>
