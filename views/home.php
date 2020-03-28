ffe5df1d3f430bd1bcda486c9777b72db7435de7
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-28 18:49:47 +0100

    Added script links and id on main for the sidenav

--- views/home.php
+++ views/home.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta author="Thophile">
     <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/all.css">
+    <script src="/assets/js/main.js"></script>
+    <script src="/assets/js/navbar.js"></script>
     <title><?= $title ?></title>
 </head>
 <body>
     <header>
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/navbar.html'?>
     </header>
-    <main></main>
+    <main id="main"></main>
 </body>
 </html>
