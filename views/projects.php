15e3063ff8727253caadd2df2e06fbd46fc5d685
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-29 23:25:30 +0200

    Added banner and projects section

--- views/projects.php
+++ views/projects.php
@@ -15,7 +15,70 @@
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/navbar.html'?>
     </header>
     <main>
-        
+        <section id="projects" class="section-row">
+            <div class="col-70">
+                <h1>
+                    Here you can see my projects
+                </h1>
+                <form action="javascript:void(0)">
+                    <button>All</button>
+                    <button>Developpement</button>
+                    <button>Network</button>
+                    <button>Other</button>
+                </form>
+            </div>
+            <div class="col-30">
+                   
+            </div>
+        </section>
+        <!-- php generated part -->
+        <section class="section-row projects deskapp developpement">
+        <div class="col-70">
+                <h1>
+                    Electron application "Deskapp"
+                </h1>
+                <h2>
+                    in category Developpement
+                </h2>
+            </div>
+            <div class="col-30">
+                <a>
+                    See more
+                </a>
+            </div>
+            </div>
+        </section>
+        <section class="section-row projects other">
+            <div class="col-70">
+                <h1>
+                    Classic car restoration
+                </h1>
+                <h2>
+                    in category Other
+                </h2>
+            </div>
+            <div class="col-30">
+                <a>
+                    See more
+                </a>
+            </div>
+        </section>
+        <section class="section-row projects network">
+            <div class="col-70">
+                <h1>
+                    Project network title
+                </h1>
+                <h2>
+                    in category x
+                </h2>
+            </div>
+            <div class="col-30">
+                <a>
+                    See more
+                </a>
+            </div>
+        </section>        
+        <!-- end of php generated prts -->
     </main>
     <footer>
         <div class="row">
