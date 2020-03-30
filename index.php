b69a24b4bbb0c98b3238d32071870116f49d90b0
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-30 20:51:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,29 +4,36 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/objects/Request.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/objects/Router.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/objects/Database.php';
 
+//Explicit enough
 $db = new Database();
 $router = new Router(new Request, $db);
 
+//Home
 $router->get('/', function() {
     $title = 'Thophile\'s Website';
     include_once 'views/home.php';
 });
 
-
+//All projects
 $router->get('/projects', function($request, $db) {
   $title = 'Thophile\'s Website | Projects';
   $projects = $db->getProjects();
   include_once 'views/projects.php';
 });
+
+//View individual project
 $router->get("/project", function($request, $db){
   $project = $db->getProject($_GET['id']);
-  $title = "Thophile\'s Website | {$project['h1']}";
+  $title = "Thophile's Website | {$project['h1']}";
   include_once 'views/project.php';
 });
 
-
-$router->post('/data', function($request) {
-
-  return json_encode($request->getBody());
-});
+/**
+ * post request template
+ *$router->post('/data', function($request) {
+ *
+ *  return json_encode($request->getBody());
+ *});
+ * 
+ */
 
