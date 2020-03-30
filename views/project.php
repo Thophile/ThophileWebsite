2f148e68ef27f373f0f2f1c5bdf07cc3d1add944
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-03-30 16:37:23 +0200

    Added template to project

---
+++ views/project.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Thophile">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/all.css">
+    <script src="/assets/js/main.js"></script>
+    <script src="/assets/js/navbar.js"></script>
+    <title><?= $title ?></title>
+</head>
+<body>
+    <header>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/navbar.html'?>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <div class="row">
+            <div class="meta">
+                Copyright
+            </div>
+            <div class="meta">
+                Terms
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
