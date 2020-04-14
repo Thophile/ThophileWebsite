8a393f3cfd9894012224784ce58ddd31b99f5fd4
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-14 15:33:02 +0200

    Create project.js with display image function
    display an image from a childnode array and from an index, uses custom attribute "label" to write the label too, indicate it's postion on the array by filling dots

---
+++ assets/js/project.js
@@ -0,0 +1,24 @@
+var images
+var label
+var index = 0
+
+ready(function (){
+    displayImage(0)
+})
+
+function displayImage(index){
+    images = document.querySelectorAll('#images img')
+    label = document.getElementById('img_label')
+    
+    images.forEach(element => {
+        element.className = ""
+    });
+    document.querySelectorAll(".dots").forEach(element => {
+        element.className = "dots"
+    })
+
+    images[index].className = "active"
+    document.querySelectorAll(".dots")[index].className ="dots active"
+
+    label.innerHTML = images[index].getAttribute('label')
+}
