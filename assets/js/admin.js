48e9955e5df6dc460f0c75148d0d54232a657859
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-20 10:39:08 +0200

    Function : adding event listerner at initialisation

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,3 +1,24 @@
+ready(function(){
+    document.querySelectorAll(".link_remove").forEach(element => {
+        element.addEventListener("click",link_remove)
+    });
+    document.querySelectorAll(".links_add").forEach(element => {
+        element.addEventListener("click",link_add)
+    });
+    document.querySelectorAll(".section_remove").forEach(element => {
+        element.addEventListener("click",section_remove)
+    });
+    document.querySelectorAll(".section_add").forEach(element => {
+        element.addEventListener("click",section_add)
+    });
+    document.querySelectorAll(".paragraphs_remove").forEach(element => {
+        element.addEventListener("click",paragraphs_remove)
+    });
+    document.querySelectorAll(".paragraphs_add").forEach(element => {
+        element.addEventListener("click",paragraphs_add)
+    });
+    
+})
 function parseForm(){
     $project = new Object()
 }
