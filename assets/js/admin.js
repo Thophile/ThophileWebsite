98e3dbc67c14d2b150c00a25e416e410f3e9503c
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-22 15:03:19 +0200

    Formatting document

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,33 +1,36 @@
-ready(function(){
+ready(function () {
     //initialise event 
     document.querySelectorAll(".link_remove").forEach(element => {
-        element.addEventListener("click",linkRemove)
+        element.addEventListener("click", linkRemove)
     });
     document.querySelectorAll(".link_add").forEach(element => {
-        element.addEventListener("click",linkAdd)
+        element.addEventListener("click", linkAdd)
     });
     document.querySelectorAll(".section_remove").forEach(element => {
-        element.addEventListener("click",sectionRemove)
+        element.addEventListener("click", sectionRemove)
     });
     document.querySelectorAll(".section_add").forEach(element => {
-        element.addEventListener("click",section_add)
+        element.addEventListener("click", section_add)
     });
     document.querySelectorAll(".paragraphs_remove").forEach(element => {
-        element.addEventListener("click",paragraphsRemove)
+        element.addEventListener("click", paragraphsRemove)
     });
     document.querySelectorAll(".paragraphs_add").forEach(element => {
-        element.addEventListener("click",paragraphsAdd)
+        element.addEventListener("click", paragraphsAdd)
     });
     document.querySelectorAll(".image_add").forEach(element => {
-        element.addEventListener("click",imageAdd)
+        element.addEventListener("click", imageAdd)
     });
     document.querySelectorAll(".image_preview i.fa-minus").forEach(element => {
-        element.addEventListener("click",imageRemove)
+        element.addEventListener("click", imageRemove)
     });
     document.querySelectorAll(".image_file").forEach(element => {
-        element.addEventListener("change",previewImage)
+        element.addEventListener("change", previewImage)
     });
-    
+    document.querySelectorAll('.bodyline a.to-validate').forEach(element => {
+        element.addEventListener('click',validate)
+    });
+
 })
 
 //ask user to confirm that he wants to delete a project
@@ -44,7 +47,7 @@ function validate(event) {
 
 
 //turn form data into json
-function parseForm(){
+function parseForm() {
     //retrieve dta
     var project = new Object()
 
@@ -52,36 +55,36 @@ function parseForm(){
     var urlparameters = window.location.search //get full string
     var parameters = new URLSearchParams(urlparameters)
     project.id = parameters.get('id')
-    
+
     project.title = document.querySelector('input[name=title]').value
     project.category = document.querySelector('input[name=category]').value
     project.banner_image = document.querySelector('input[name=style]').value
-    
-    
+
+
     //image
     project.images = []
     var data = new FormData()
     document.querySelectorAll('#_image .image_preview').forEach(element => {
-        
+
         //if an img has been loaded/selected
-        if(element.querySelector('img').src != ""){
+        if (element.querySelector('img').src != "") {
             var image = new Object()
-            
+
             //save the filename
-            if(element.querySelector('input[type=file]').files.length == 0){
+            if (element.querySelector('input[type=file]').files.length == 0) {
                 image.filename = element.querySelector('img').src.split(/(\\|\/)/g).pop()
-            }else{
+            } else {
                 image.filename = element.querySelector('input[type=file]').files[0].name
                 data.append('file[]', element.querySelector('input[type=file]').files[0])
             }
-            
+
             image.label = element.querySelectorAll('input')[1].value
-            
+
             project.images.push(image)
         }
     });
-    
-    
+
+
     //links
     project.links = []
     document.querySelectorAll('#_links div').forEach(element => {
@@ -90,32 +93,32 @@ function parseForm(){
         link.href = element.querySelectorAll('input')[1].value
         project.links.push(link)
     });
-    
-    
+
+
     //article
     project.article = []
     document.querySelectorAll('#_article .article_section').forEach(element => {
         var section = new Object()
         section.title = element.querySelector('.article_title input[type=text').value
         section.paragraphs = []
-        element.querySelectorAll('.article_paragraphs textarea').forEach(p =>{
+        element.querySelectorAll('.article_paragraphs textarea').forEach(p => {
             section.paragraphs.push(p.value)
         })
         project.article.push(section)
     })
-    
+
     data.append('project', JSON.stringify(project))
 
     //send to create or update
     var request = new XMLHttpRequest();
     request.open('post', '/upload');
-    
-    // AJAX request finished event
-    request.addEventListener('load', function(e) {
 
-	// request.response will hold the response from the server
-    console.log(request.response);
-    document.getElementById('status').innerHTML = 'Status : ' + request.status + ', ' + request.statusText
+    // AJAX request finished event
+    request.addEventListener('load', function (e) {
+
+        // request.response will hold the response from the server
+        console.log(request.response);
+        document.getElementById('status').innerHTML = 'Status : ' + request.status + ', ' + request.statusText
     });
 
     // send POST request to server side script
@@ -123,167 +126,167 @@ function parseForm(){
 }
 
 //Images
-function previewImage(event){
+function previewImage(event) {
     //get the input that emitted the event
     var source = event.target || event.srcElement
     var file = source.files[0]
     const reader = new FileReader()
 
     //get the div parent
-    while(source.className !== "image_preview"){
+    while (source.className !== "image_preview") {
         source = source.parentElement
     }
-    
+
     //swap img and placeholder visibility
     source.children[0].style.display = "block" //img
     source.children[1].style.display = "none" //placeholder
 
     //get the img of the div and load the image in it
     var preview = source.children[0]
-    reader.addEventListener("load" ,function () {
+    reader.addEventListener("load", function () {
         //convert file to string
         preview.src = reader.result
     }, false)
 
-    if(file) reader.readAsDataURL(file);
+    if (file) reader.readAsDataURL(file);
 
 }
-function imageRemove(event){
+function imageRemove(event) {
     var source = event.target || event.srcElement
 
     //handle nested source
-    while(source.className !== "image_preview"){
+    while (source.className !== "image_preview") {
         source = source.parentElement
     }
-    source.parentElement.removeChild(source) ;
+    source.parentElement.removeChild(source);
 }
-function imageAdd(event){
+function imageAdd(event) {
     var source = event.target || event.srcElement
     while (source.tagName != "BUTTON") {
         source = source.parentElement
     }
-    var div = create("DIV", {className: "image_preview"})
-        div.appendChild(create("IMG"))
+    var div = create("DIV", { className: "image_preview" })
+    div.appendChild(create("IMG"))
 
-        let i = create("I", {className : "fas fa-6x fa-upload"})
-        div.appendChild(i)
+    let i = create("I", { className: "fas fa-6x fa-upload" })
+    div.appendChild(i)
 
-        let input = create("INPUT", {className : "image_file", type : "file"})
-        input.addEventListener("change",previewImage)
-        div.appendChild(input)
+    let input = create("INPUT", { className: "image_file", type: "file" })
+    input.addEventListener("change", previewImage)
+    div.appendChild(input)
 
-        input = create("INPUT", {type : "text", placeholder: "Label"})
-        div.appendChild(input)
+    input = create("INPUT", { type: "text", placeholder: "Label" })
+    div.appendChild(input)
 
-        i = create("I", {className : "fas fa-minus"})
-        i.addEventListener("click",imageRemove)
-        div.appendChild(i)
+    i = create("I", { className: "fas fa-minus" })
+    i.addEventListener("click", imageRemove)
+    div.appendChild(i)
 
-    document.getElementById("_image").insertBefore(div,source)
+    document.getElementById("_image").insertBefore(div, source)
 
 }
 
 //Link
-function linkRemove(event){
+function linkRemove(event) {
     var source = event.target || event.srcElement
 
     //check if you click on the i or on the button itself
-    while(source.tagName !== "DIV"){
+    while (source.tagName !== "DIV") {
         source = source.parentElement
     }
-    source.parentElement.removeChild(source) ;
+    source.parentElement.removeChild(source);
 }
-function linkAdd(){
+function linkAdd() {
     var source = event.target || event.srcElement
-    while(source.tagName !== "BUTTON"){
+    while (source.tagName !== "BUTTON") {
         source = source.parentElement
     }
 
     var div = create("DIV")
 
-        let input = create("INPUT", {type : "text", size: "1", placeholder: "Name"})
-        div.appendChild(input)
+    let input = create("INPUT", { type: "text", size: "1", placeholder: "Name" })
+    div.appendChild(input)
 
-        div.appendChild(document.createTextNode("to : "))
+    div.appendChild(document.createTextNode("to : "))
 
-        input = create("INPUT", {type : "text", size: "1", placeholder: "Link"})
-        div.appendChild(input)
+    input = create("INPUT", { type: "text", size: "1", placeholder: "Link" })
+    div.appendChild(input)
 
-        let button = create("BUTTON", {type : "button", className: "btn link_remove"})
-            let i = create("I", {className : "fas fa-minus"})
-        button.appendChild(i)
-        button.addEventListener("click",linkRemove)
+    let button = create("BUTTON", { type: "button", className: "btn link_remove" })
+    let i = create("I", { className: "fas fa-minus" })
+    button.appendChild(i)
+    button.addEventListener("click", linkRemove)
 
     div.appendChild(button)
-    source.parentElement.insertBefore(div,source)
+    source.parentElement.insertBefore(div, source)
 }
 
 //Articles
-function sectionRemove(event){
+function sectionRemove(event) {
     var source = event.target || event.srcElement
-    while(source.className !== "article_section"){
+    while (source.className !== "article_section") {
         source = source.parentElement
     }
-    source.parentElement.removeChild(source) ;
+    source.parentElement.removeChild(source);
 }
-function section_add(){
+function section_add() {
     var source = event.target || event.srcElement
-    while(source.tagName !== "BUTTON"){
+    while (source.tagName !== "BUTTON") {
         source = source.parentElement
     }
     //create a section
-    var div = create("DIV", {className : "article_section"})
+    var div = create("DIV", { className: "article_section" })
 
-        var div_title = create("DIV", {className : "article_title"})
-            let input = create("INPUT", {type: "text", placeholder: "Section title"})
-        div_title.appendChild(input)
+    var div_title = create("DIV", { className: "article_title" })
+    let input = create("INPUT", { type: "text", placeholder: "Section title" })
+    div_title.appendChild(input)
 
-            let button = create("BUTTON", {type: "button", className : "btn section_remove"})
-                let i = create("I", {className : "fas fa-minus"})
-            button.appendChild(i)
-            button.addEventListener("click",sectionRemove)
+    let button = create("BUTTON", { type: "button", className: "btn section_remove" })
+    let i = create("I", { className: "fas fa-minus" })
+    button.appendChild(i)
+    button.addEventListener("click", sectionRemove)
 
-        div_title.appendChild(button)
+    div_title.appendChild(button)
 
     div.appendChild(div_title)
 
-        let div_para = create("DIV", {className: "article_paragraphs"})
-            div_para.appendChild(create("textarea"))
+    let div_para = create("DIV", { className: "article_paragraphs" })
+    div_para.appendChild(create("textarea"))
     div.appendChild(div_para)
 
-        let div_action = create("DIV", {className : "article_action"})
-            button = create("BUTTON", {type: "button", className: "btn paragraphs_remove"})
-                i = create("I", {className: "fas fa-minus"})
-            button.appendChild(i)
-            button.appendChild(document.createTextNode(" Remove paragraph"))
-            button.addEventListener("click",paragraphsRemove)
+    let div_action = create("DIV", { className: "article_action" })
+    button = create("BUTTON", { type: "button", className: "btn paragraphs_remove" })
+    i = create("I", { className: "fas fa-minus" })
+    button.appendChild(i)
+    button.appendChild(document.createTextNode(" Remove paragraph"))
+    button.addEventListener("click", paragraphsRemove)
 
-        div_action.appendChild(button)
-        
-            button = create("BUTTON", {type: "button", className: "btn paragraphs_add"})
-                i = create("I", {className: "fas fa-plus"})
-            button.appendChild(i)
-            button.appendChild(document.createTextNode(" Add paragraph"))
-            button.addEventListener("click",paragraphsAdd)
+    div_action.appendChild(button)
 
-        div_action.appendChild(button)
-    
+    button = create("BUTTON", { type: "button", className: "btn paragraphs_add" })
+    i = create("I", { className: "fas fa-plus" })
+    button.appendChild(i)
+    button.appendChild(document.createTextNode(" Add paragraph"))
+    button.addEventListener("click", paragraphsAdd)
+
+    div_action.appendChild(button)
+
     div.appendChild(div_action)
 
-    source.parentElement.insertBefore(div,source)
+    source.parentElement.insertBefore(div, source)
 }
 
 
-function paragraphsRemove(event){
+function paragraphsRemove(event) {
     var source = event.target || event.srcElement
-    while(source.className !== "article_section"){
+    while (source.className !== "article_section") {
         source = source.parentElement
     }
     source.children[1].removeChild(source.children[1].lastElementChild)
 }
-function paragraphsAdd(event){
+function paragraphsAdd(event) {
     var source = event.target || event.srcElement
-    while(source.className !== "article_section"){
+    while (source.className !== "article_section") {
         source = source.parentElement
     }
     source.children[1].appendChild(document.createElement("TEXTAREA"))
