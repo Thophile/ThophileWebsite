c3055c331d63dbb52baeb3876bda858f61b44d6e
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-23 15:10:15 +0200

    added header to token and token format check

--- objects/Authenticator.php
+++ objects/Authenticator.php
@@ -10,32 +10,41 @@ class Authenticator{
     public function generateToken(){
         
         //set expiration time at t + 2hour and encode it to b64
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $b64header = base64_encode($header);
         $exp = json_encode(['expire' => (time() + 7200)]);
         $b64exp = base64_encode($exp);
 
         //generate signature and encode it to b64
-        $signature = hash_hmac('sha256', $b64exp, env('APP_KEY'), true);
+        $signature = hash_hmac('sha256', $b64header . '.' . $b64exp, env('APP_KEY'), true);
         $b64sign = base64_encode($signature);
 
-        return $token = $b64exp .".". $b64sign;
+        return $token = $b64header . "." . $b64exp . "." . $b64sign;
     }
 
     //validate a token
     public function validateToken($token){
+
         //cut the token
         $data = explode('.', $token);
 
-        //decode data
-        $exp = base64_decode($data[0]);
-        $sign = base64_decode($data[1]);
+        //if the token has 3 parts
+        if(sizeof($data) === 3 ){
 
-        //if the token has expire dont validate it
-        if(json_decode($exp)->expire > time()){
-            //hash data
-            $hash = hash_hmac('sha256', $data[0], env('APP_KEY'), true);
+            //decode data
+            $header = base64_decode($data[0]);
+            $exp = base64_decode($data[1]);
+            $sign = base64_decode($data[2]);
 
-            //using non time attack vulnerable comparision
-            return hash_equals($hash, $sign);
+            //if the token has not expired
+            if(json_decode($exp)->expire > time()){
+
+                //create hash from user data
+                $hash = hash_hmac('sha256', $data[0] . '.' . $data[1], env('APP_KEY'), true);
+    
+                //using non time attack vulnerable comparision to check if token is valid
+                return hash_equals($hash, $sign);
+            }
         }
         return false;
 
