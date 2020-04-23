9c182a66d6c45db5a232f7f4ce924b5a23e56ce1
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-23 10:48:32 +0200

    Created validatepassword,generateToken and validateToken function declaration

--- objects/Authenticator.php
+++ objects/Authenticator.php
@@ -1,4 +1,18 @@
 <?php
 class Authenticator{
-    
+
+    //validate the password with the env app_password
+    public function validatePassword($password){
+
+    }
+
+    //generate a token
+    public function generateToken(){
+
+    }
+
+    //validate a token
+    public function validateToken($token){
+
+    }
 }
