dd14c05b69c0e4386d2b09a5c2af901cfb0547ac
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-04-23 13:02:14 +0200

    generateToken function

--- objects/Authenticator.php
+++ objects/Authenticator.php
@@ -8,7 +8,16 @@ class Authenticator{
 
     //generate a token
     public function generateToken(){
+        
+        //set expiration time at t + 2hour and encode it to b64
+        $exp = json_encode(['expire' => (time() + 7200)]);
+        $b64exp = base64_encode($exp);
 
+        //generate signature and encode it to b64
+        $signature = hash_hmac('sha256', $b64exp, env('APP_KEY'), true);
+        $b64sign = base64_encode($signature);
+
+        return $token = $b64exp .".". $b64sign;
     }
 
     //validate a token
