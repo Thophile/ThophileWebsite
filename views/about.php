5f1eaec1e59edf9e44e6dc64423a655b061e4531
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-04 09:26:19 +0200

    Created empty about page

---
+++ views/about.php
@@ -0,0 +1,34 @@
+<?php
+if(__FILE__ == $_SERVER['SCRIPT_FILENAME']){
+    include_once $_SERVER['DOCUMENT_ROOT'].'/errors/403.html';
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Thophile">
+    <link rel="stylesheet" href="/assets/css/about.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/all.css">
+    <script src="/assets/js/main.js"></script>
+    <script src="/assets/js/navbar.js"></script>
+    <title><?= $title ?></title>
+</head>
+
+<body>
+    <header>
+        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/navbar.php'?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'?>
+    </footer>
+</body>
+
+</html>
