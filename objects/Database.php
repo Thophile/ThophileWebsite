f133399361e9db4a927b1dbb51effe2a1d0e996c
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-05 15:35:51 +0200

    Update Database.php

--- objects/Database.php
+++ objects/Database.php
@@ -187,7 +187,6 @@ class Database{
     }
     public function hit($page){
 
-    //Get values from server
         //Add the id to "page" if it's a project page
         if($page === "/project") $page .= " ".$_GET['id'];
         //Referer checks
