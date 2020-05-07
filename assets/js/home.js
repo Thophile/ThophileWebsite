23b5c5df8654482dc25a626689f0f1cae9da3c5b
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-07 11:39:46 +0200

    added ease function for the scroll

--- assets/js/home.js
+++ assets/js/home.js
@@ -27,5 +27,23 @@ window.addEventListener('wheel', function(event)
 
 });
 
+window.smoothScrollTo = function(endY, duration) {
+    startY = window.scrollY || window.pageYOffset,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
 
-}
+    // Easing function
+    let easeInOutQuart = function(time, from, distance, duration) {
+        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+
+    let timer = window.setInterval(function() {
+        let time = new Date().getTime() - startTime,
+        newY = easeInOutQuart(time, startY, distanceY, duration);
+        if (time >= duration) {
+            window.clearInterval(timer);
+        }
+        window.scrollTo(0, newY);
+    }, 1000 / 60); // 60 fps
+};
