df14f5fe75c74556b39af3aee21a2b329043d8d6
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-11 13:47:01 +0200

    Documented landing.js

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,3 +1,13 @@
+/**
+ * Landing page script
+ * @Author Thophile
+ * @license MIT
+ */
+
+/**
+ * Rickroll user after 4 seconds if a wrong password is typed in
+ * @see main.js
+ */
 ready(function () {
     if (document.getElementsByClassName("error").length !== 0) {
 
@@ -5,4 +15,4 @@ ready(function () {
             document.getElementById('landing').innerHTML = '<iframe height="100%" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&mute=0"></iframe>'
         }, 4000)
     }
-})
+});
