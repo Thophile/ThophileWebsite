a5af3d0a3e10e8281605a9bbdd0be3f5f5fb8253
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-11 13:47:10 +0200

    Documented main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+/**
+ * Main function list
+ * @Author Thophile
+ * @license MIT
+ */
+
+/**
+ * Add a function to be called when document is ready
+ * @param {function} f the function to be called when ready
+ */
 function ready(f) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -7,7 +17,12 @@ function ready(f) {
         document.addEventListener("DOMContentLoaded", f);
     }
 }
-//shortcut for document.createElement and attribut association
+/**
+ * Create a dom element with a list of properties to be set
+ * 
+ * @param {string} element 
+ * @param {Object <string, string>} properties 
+ */
 function create(element, properties) {
     //create element
     var element = document.createElement(element);
