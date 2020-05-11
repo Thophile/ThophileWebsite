256fb74c668ba765194ab3cc88ceb149d262944a
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-11 13:47:42 +0200

    Documented projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,11 +1,19 @@
+/** @type {Array <Element>} */
 var elements;
 
+/**
+ * Show all projects
+ */
 function showAll() {
     elements = document.querySelectorAll('.projects');
     for (var i = 0, max = elements.length; i < max; i++) {
         elements[i].style.display = "flex";
     }
 }
+
+/**
+ * Show projects with category attribute as "Developpement"
+ */
 function showDev() {
     elements = document.querySelectorAll('.projects');
     for (var i = 0, max = elements.length; i < max; i++) {
@@ -16,6 +24,10 @@ function showDev() {
         elements[i].style.display = "flex";
     }
 }
+
+/**
+ * Show projects with category attribute as "Network"
+ */
 function showNet() {
     elements = document.querySelectorAll('.projects');
     for (var i = 0, max = elements.length; i < max; i++) {
@@ -26,6 +38,10 @@ function showNet() {
         elements[i].style.display = "flex";
     }
 }
+
+/**
+ * Show projects with category attribute as "Other"
+ */
 function showOth() {
     elements = document.querySelectorAll('.projects');
     for (var i = 0, max = elements.length; i < max; i++) {
