07db737c419b4ee1ddcd48fc862d26aae1f62bd3
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-11 09:42:12 +0200

    documented cli.php

--- cli.php
+++ cli.php
@@ -1,12 +1,22 @@
 <?php
+//Ensure that the script is called is console line
 if(php_sapi_name() !== 'cli') { 
     include_once $_SERVER['DOCUMENT_ROOT'].'/errors/403.html';
     die();
 }
 
+/**
+ * Hash a password 
+ * @param  string $password The password to be hashed
+ */
 function generatePassword($password){
     echo password_hash($password, PASSWORD_DEFAULT);
 }
+
+/**
+ * Generate a secret key
+ * @param int $size The size of the key to be generated
+ */
 function generateSecret($size){
     $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $secret = '';
