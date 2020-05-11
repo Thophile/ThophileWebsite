b536969882caf719c21116b3312dd62ca0aa154f
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2020-05-11 14:52:34 +0200

    Updated about page
    added other networks links ,
    changed lorem to true text

--- views/about.php
+++ views/about.php
@@ -31,24 +31,28 @@ if(__FILE__ == $_SERVER['SCRIPT_FILENAME']){
             <section id="studies">
                 <h2>Studies</h2>
                 <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate nulla ipsum dolore pariatur maxime distinctio, ut praesentium repellendus blanditiis facere laborum dolores rem beatae veniam, aperiam eveniet corporis? Beatae, nesciunt.
-                Voluptatibus dolore eius quidem beatae dolores qui adipisci blanditiis quas, a ex quasi. Minima modi a dicta, ullam ab voluptatibus debitis eum possimus veniam id eveniet, recusandae, dolor aut nostrum?
-                Deserunt nisi cupiditate odit quibusdam rem officiis, ea repellat illo doloribus, quo obcaecati aliquam aperiam labore, magni accusamus vitae? Maxime nihil ullam animi iste. Ratione iste eos reiciendis optio dolorem.
-                Consectetur aliquam facere quidem quaerat officiis molestias autem, earum excepturi minima ut perspiciatis corporis dolores aliquid saepe a sit perferendis odit. Eveniet soluta velit culpa maiores corporis, saepe temporibus quod?
-                </p>
+                I have always wanted to be useful to the society, my natural talent for logics oriented me to an IT formation after a "scientific Baccalauréat" 
+                I think IT have and will have a huge place in the society as a tool to help everyone and make any task easier and as an entertainment tool too.
+                With so said roll IT is a perfect choice to use the most of my capacity while doing something good for helping people</p>
             </section>
 
             <section id="hobbies">
                 <h2>Hobbies</h2>
                 <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sit amet assumenda tenetur velit vel consectetur maiores earum? Sed ut ipsum delectus animi! Assumenda, perferendis officia? Magnam architecto inventore maiores!
-                Commodi minus, dolore hic numquam dicta fuga libero ratione maxime illum dolores et porro odit, distinctio, ea debitis saepe perferendis? Facere inventore, recusandae illo incidunt maxime veritatis molestias deserunt vitae.
-                Laboriosam enim recusandae, commodi eaque molestias modi eligendi cumque omnis assumenda vitae ratione veritatis, cum fugiat unde laborum. Quod quas voluptatibus dicta tenetur qui vel a maiores obcaecati. Doloremque, incidunt.
-                Magnam, nihil assumenda porro illo enim labore necessitatibus, nulla distinctio libero ipsum, nostrum ut aspernatur odio architecto iusto! Eligendi delectus, accusamus odit distinctio sed vitae soluta doloremque ratione voluptatum expedita?
+                I like to do the most I can by myself.
+                It all started when I was a first year primary student when was authorized for the first time to use an axe saw and I never stopped working on manual project during my free time since ! 
+                
+                I like to learn and I like useless or beautifull gadget but i'm not that rich so I can't buy all the thing I find fun or pretty. 
+                Most of the time I end up watching tutorials and building them myself for a 10th of the price. Sometimes it cost me 10 times the price but I always end up learning something new and having a lot of fun !
                 </p>
             </section>
 
-                <a href="mailto:" class="btn pills">Contact me</a>
+            <section id="contacts">
+                <h2>Contact</h2>
+                <a href="mailto:" class="btn pills"> <i class="fas fa-envelope"></i> My mail</a>
+                <a href="https://www.linkedin.com/in/th%C3%A9ophile-mont%C3%A9mont-24067a192/" class="btn pills"> <i class="fab fa-linkedin"></i> My LinkedIn</a>
+                <a href="https://twitter.com/TheophileMNT" class="btn pills"> <i class="fab fa-twitter-square"></i> My twitter</a>
+            </section>
 
         </article>
     
