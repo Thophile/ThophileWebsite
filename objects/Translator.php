d9029caa85450d596102414fa9e18c52ebf991ec
Thophile <montemonttheophile@gmail.com>
2020-06-10 21:02:46 +0200

    [Add] Added translate function in constructor with recursive search

--- objects/Translator.php
+++ objects/Translator.php
@@ -14,15 +14,34 @@ if(__FILE__ == $_SERVER['SCRIPT_FILENAME']){
 class Translator
 {
     private $dictionary = [];
+    private $locale;
+    
+    function __construct(){
 
-    function __construct($locale){
+        //get default locale
+        $this->locale = env("DEFAULT_LOCALE");
+
+        //get dictionary data
         if(!file_exists($locale.".json")){
             $this->dictionary = json_decode(file_get_contents($locale.".json"), true);
         }
+
+
+        /**
+         * @param String $textCode the text code that point on the text we need
+         */
+        function translate(String $textCode){
+            //recursively search text in nested array corresponding to textcode tree
+            $branches = explode(".", $textCode);
+            $text = $this->dictionary;
+            foreach ($branches as $branche) {
+                $text = $text[$branch];
+            }
+            echo $text;
+            return;
+        }
     }
 
-    function translate(String $textCode){
-        return $dictionary[$textCode];
-    }
+    
 
 }
